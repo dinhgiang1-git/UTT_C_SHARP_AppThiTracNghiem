e44ca24ea9fb5da89c15b02bcf02eed28a20d770
Thinh Chu <chuthinh2104@gmail.com>
2025-04-07 03:20:57 +0700

    SQL new connect

--- Form/fSinhVienMain.cs
+++ Form/fSinhVienMain.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using DocumentFormat.OpenXml.Spreadsheet;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using System.Configuration;
 
 namespace ThiTracNghiem
 {
