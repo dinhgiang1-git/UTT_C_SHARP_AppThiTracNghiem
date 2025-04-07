9b97998960cb28846dea10ecea8be981177b3082
ĐINH ĐỨC GIANG <dinhgiang1.edu@gmail.com>
2025-04-08 00:16:08 +0700

    Revert "Legendary Merge..."

--- Form/fMain.cs
+++ Form/fMain.cs
@@ -2091,10 +2091,5 @@ namespace ThiTracNghiem
             string maMonHoc = tcdcbMonHoc.SelectedValue.ToString();
             LoadCombox_DeThi(maMonHoc);
         }
-
-        private void thôngTinTàiKhoảnToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-           
-        }
     }
 }
